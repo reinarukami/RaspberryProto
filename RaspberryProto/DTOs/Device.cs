a65aa650f3cd8f5c7a7b212f5be19fdbd30829a1
Robin <robin.sumalde.ivp@gmail.com>
2018-09-05 20:08:22 +0800

    Initial Commit

---
+++ RaspberryProto/DTOs/Device.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaspberryProto.DTOs
+{
+	public partial class Devices
+	{
+
+		public int id { get; set; }
+
+		public string name { get; set; }
+
+		public string status { get; set; }
+
+	}
+
+}
--- RaspberryProto/MainPage.xaml.cs
+++ RaspberryProto/MainPage.xaml.cs
@@ -1,39 +1,84 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using RaspberryProto.DTOs;
+using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
+using System.Net.Http;
+using System.Threading.Tasks;
 using Windows.Devices.Gpio;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
 namespace RaspberryProto
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
-    public sealed partial class MainPage : Page
+	/// <summary>
+	/// An empty page that can be used on its own or navigated to within a Frame.
+	/// </summary>
+	public sealed partial class MainPage : Page
     {
+		private const string url = "http://devicecontrolapi.azurewebsites.net/Device/";
 		private bool red_state = false;
 		private bool green_state = false;
+		private bool yellow_state = false;
+		private DispatcherTimer timer;
 		private const int RED = 5;
-		private const int GREEN = 18;
+		private const int GREEN = 6;
+		private const int YELLOW = 13;
 		private GpioPin redPin;
 		private GpioPin greenPin;
+		private GpioPin yellowPin;
 
 		public MainPage()
         {
             this.InitializeComponent();
-			InitGpio();
+			InitializeTimer();
+			//InitGpio();
+		}
+
+		private void InitializeTimer()
+		{
+			timer = new DispatcherTimer();
+			timer.Interval = TimeSpan.FromSeconds(1);
+			timer.Tick += GetHostRequest;
+			timer.Start();
+		}
+
+		private async void GetHostRequest(object sender, object e)
+		{
+			TimeLabel.Text = DateTime.Now.ToString();
+
+			try
+			{
+
+				HttpClient client = new HttpClient();
+
+				HttpResponseMessage response = await client.GetAsync(url);
+				if (response.IsSuccessStatusCode)
+				{
+					string stringstatus = await response.Content.ReadAsStringAsync();
+					var test = JsonConvert.DeserializeObject<List<Devices>>(stringstatus);
+
+					if (stringstatus == "true")
+					{
+						GreenStatusLabel.Text = "Status:ON";
+					}
+
+					else
+					{
+						GreenStatusLabel.Text = "Status:Off";
+					}
+
+				}
+
+			}
+
+			catch(Exception ex)
+			{
+				
+			}
+
+
 		}
 
 		private void InitGpio()
@@ -41,10 +86,13 @@ namespace RaspberryProto
 			var gpio = GpioController.GetDefault();
 			redPin = gpio.OpenPin(RED);
 			greenPin = gpio.OpenPin(GREEN);
+			yellowPin = gpio.OpenPin(YELLOW);
 			redPin.Write(GpioPinValue.Low);
 			greenPin.Write(GpioPinValue.Low);
+			yellowPin.Write(GpioPinValue.Low);
 			redPin.SetDriveMode(GpioPinDriveMode.Output);
 			greenPin.SetDriveMode(GpioPinDriveMode.Output);
+			yellowPin.SetDriveMode(GpioPinDriveMode.Output);
 		}
 
 		private void Green_Click(object sender, RoutedEventArgs e)
@@ -62,9 +110,10 @@ namespace RaspberryProto
 				GreenStatusLabel.Text = "Status:Off";
 			}
 		}
+	
 
 		private void Red_Click(object sender, RoutedEventArgs e)
-		{
+		{		
 			if (red_state == false)
 			{
 				redPin.Write(GpioPinValue.High);
@@ -79,10 +128,27 @@ namespace RaspberryProto
 			}
 		}
 
+
+		private async void Yellow_Click(object sender, RoutedEventArgs e)
+		{
+			yellow_state = !yellow_state;
+
+		    while (yellow_state == true)
+			{
+				yellowPin.Write(GpioPinValue.High);
+				await Task.Delay(500);
+				yellowPin.Write(GpioPinValue.Low);
+				await Task.Delay(500);
+				YellowStatusLabel.Text = "Status:ON";
+			}
+		}
+
 		private void Page_Unloaded(object sender, RoutedEventArgs e)
 		{
 			redPin.Dispose();
 			greenPin.Dispose();
 		}
+
+
 	}
 }
